a11dca02762eaf9a6c86ffa8c2a96f02818650ae
whiletrueplusplus <112478370+whiletrueplusplus@users.noreply.github.com>
2022-09-08 17:05:11 +0900

    [Silver V] Title: 소트인사이드, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int num=scan.nextInt();
+		int s=0;
+		int temp=num;
+		while(temp!=0)
+		{
+			temp/=10;
+			s++;
+		}
+		int[] array=new int[s];
+		for(int i=0;i<s;i++)
+		{
+			array[i]=num%10;
+			num/=10;
+		}
+		int j=1;
+		int answer=0;
+		Arrays.sort(array);
+		for(int i=0;i<s;i++)
+		{
+			answer+=array[i]*j;
+			j*=10;
+		}
+		System.out.println(answer);
+	}
+
+}
